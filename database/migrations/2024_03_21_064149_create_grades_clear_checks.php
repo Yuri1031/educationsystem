8f17bf2641c5e00739c72b490a2046b338bc044a
Matsuoka0711 <taiki1544.0711@gmail.com>
2024-03-22 10:01:23 +0900

    migration file1

---
+++ database/migrations/2024_03_21_064149_create_grades_clear_checks.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades_clear_checks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
+            $table->bigIncrements('grade_id');
+            $table->tinyInteger('clear_flg')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades_clear_checks');
+    }
+};
