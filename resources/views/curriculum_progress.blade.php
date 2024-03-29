8fbfea6e07777ed9b14481cad32c9117f39c9c5d
Matsuoka0711 <taiki1544.0711@gmail.com>
2024-03-29 11:23:30 +0900

    curriculum_progress page 1

---
+++ resources/views/curriculum_progress.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>授業進捗画面</title>
+</head>
+<body>
+  @include('user_header') <!-- 共通ヘッダーをインクルード -->
+  <a href="">←戻る</a>
+
+  <div class="profile">
+    <div class="profileImage">
+      <img src="" alt="プロフィール画像です">
+    </div>
+    <div class="profileText">
+      <h2>⚪︎⚪︎さんの授業進捗</h2>
+      <p>現在の学年 : </p>
+    </div>
+  </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/curriculum_progress', function (){
+    return view('curriculum_progress');
+});
