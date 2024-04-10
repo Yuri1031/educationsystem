06b0ceaa6fb1fffc551c2057500c29e2e6bb7890
Yuri1031 <m17006ya@gmai.com>
2024-04-10 23:20:41 +0900

    edit 2024/4/10

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'name_kana' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'profile_image' => 'image|mimes:jpeg,png,jpg,gif',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'name_kana' => $data['name_kana'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'profile_image' => 'image|mimes:jpeg,png,jpg,gif',
+        ]);
+
+        // プロフィール画像がアップロードされている場合は保存する
+        if (isset($data['profile_image'])) {
+            // アップロードされたファイルを保存し、そのパスをデータベースに保存する
+            $profileImagePath = $data['profile_image']->store('profile_images', 'public');
+            $user->update(['profile_image' => $profileImagePath]);
+        }
+
+         return $user;
+    }
+}
---
+++ resources/views/top.blade.php
@@ -0,0 +1,9 @@
+@include('user_header')
+<a href="">戻る</a>
+
+<body>
+    <!--バナー画像-->
+    <div></div>
+    <!--お知らせ-->
+    <div></div>
+</body>
